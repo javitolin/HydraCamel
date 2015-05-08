2d30d1de1edfa784ac8d923ac20600873068040e
papigers <papigers93@gmail.com>
2015-05-08 15:14:11 +0300

    begin testing black box need fucking pics

--- DebugFilters/BlackBoxDebugAlgorithm.cpp
+++ DebugFilters/BlackBoxDebugAlgorithm.cpp
@@ -11,13 +11,20 @@ using namespace cv;
 using namespace std;
 
 /// Global variables
-Mat src;
+Mat src, bw;
+int thresh = 100;
+int max_thresh = 255;
+RNG rng(12345);
 
 /** Function Headers */
 void abs(int, void*);
 /** @function main */
 int main( int argc, char** argv )
 {
+	src = imread( "blackbox.png" );
+
+	abs(0,0);
+	/*
 	DIR *dp;
 	struct dirent *dirp;
 	if((dp  = opendir(".")) == NULL) {
@@ -60,173 +67,92 @@ int main( int argc, char** argv )
 	printf("We were correct in %f%% of the cases\n",successRate);
 
 	waitKey(0);
+	 */
 	return 0;
 }
 
-bool lineCompare(Vec2f line1, Vec2f line2){
-	float rho = line1[0], theta = line1[1];
-	double x1, x2;
-	double a = cos(theta), b = sin(theta);
-	double x0 = a*rho;
-	x1 = cvRound(x0 + 1000*(-b));
-	rho = line2[0];
-	theta = line2[1];
-	a = cos(theta);
-	b = sin(theta);
-	x0 = a*rho;
-	x2 = cvRound(x0 + 1000*(-b));
-	return x1<x2;
+void thresh_callback(int, void* ){
+	Mat threshold_output;
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+
+	/// Detect edges using Threshold
+	threshold( bw, threshold_output, thresh, 255, THRESH_BINARY );
+	/// Find contours
+	findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+	/// Approximate contours to polygons + get bounding rects and circles
+	vector<vector<Point> > contours_poly( contours.size() );
+	vector<Rect> boundRect( contours.size() );
+	vector<Point2f>center( contours.size() );
+	vector<float>radius( contours.size() );
+
+	for( int i = 0; i < contours.size(); i++ )
+	{
+		approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+		boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+		minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+	}
+
+
+	/// Draw polygonal contour + bonding rects + circles
+	Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
+	for( int i = 0; i< contours.size(); i++ )
+	{
+		Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+		drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+		rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+		circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+	}
+
+	//distance code starts here
+
+	/// Calculate the distances to the contour
+	Mat raw_dist( src.size(), CV_32FC1 );
+
+	for( int j = 0; j < src.rows; j++ ){
+		for( int i = 0; i < src.cols; i++ ){
+			raw_dist.at<float>(j,i) = pointPolygonTest( contours[0], Point2f(i,j), true );
+		}
+	}
+
+	double minVal; double maxVal;
+	minMaxLoc( raw_dist, &minVal, &maxVal, 0, 0, Mat() );
+	minVal = abs(minVal); maxVal = abs(maxVal);
+
+	/// Depicting the  distances graphically
+	Mat dists = Mat::zeros( src.size(), CV_8UC3 );
+
+	for( int j = 0; j < src.rows; j++ ){
+		for( int i = 0; i < src.cols; i++ )
+		{
+			if( raw_dist.at<float>(j,i) < 0 ){
+				dists.at<Vec3b>(j,i)[0] = 255 - (int) abs(raw_dist.at<float>(j,i))*255/minVal;
+			}
+			else if( raw_dist.at<float>(j,i) > 0 ){
+				dists.at<Vec3b>(j,i)[2] = 255 - (int) raw_dist.at<float>(j,i)*255/maxVal;
+			}
+			else{
+				dists.at<Vec3b>(j,i)[0] = 255;
+				dists.at<Vec3b>(j,i)[1] = 255;
+				dists.at<Vec3b>(j,i)[2] = 255;
+			}
+		}
+	}
+
+	/// Show in a window
+	namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+	imshow( "Contours", drawing );
+	namedWindow( "Distance", CV_WINDOW_AUTOSIZE );
+	imshow( "Distance", dists );
 }
 
 void abs(int, void*){
-	numberOfLinesFound = 0;
-	Mat bw, cont;
 	cvtColor(src,bw, CV_RGB2GRAY);
-	//int operation = morph_operator + 2;
-	int operation = 3;
-	int morph_size = max_kernel_size;
-	Mat element = getStructuringElement( morph_elem, Size( 2*morph_size + 1, 2*morph_size+1 ), Point( morph_size, morph_size ) );
-	Mat dst;
-	/// Apply the specified morphology operation
-	morphologyEx( bw, dst, operation, element );
+	blur(bw, bw, Size(3,3));
+	createTrackbar( " Threshold:", "Source", &thresh, max_thresh, thresh_callback );
+	thresh_callback( 0, 0 );
 
-	Mat edges;
-	Canny( dst, edges, lowThreshold, lowThreshold*ratio, canny_kernel_size );
-	cvtColor(edges, cedges, CV_GRAY2BGR);
-
-	vector<Vec2f> lines;
-	HoughLines(edges, lines, 1, CV_PI/180, 120, 0, 0 );
-	sort(lines, lineCompare);
-	int numOfLines = 0;
-	double groupMin, lastX = -1;
-	int currGroup = 1;
-
-	vector<Vec2f> group1 = vector<Vec2f>(), group2 = vector<Vec2f>();
-
-	for( size_t i = 0; i < lines.size(); i++ )
-	{
-		float rho = lines[i][0], theta = lines[i][1];
-		Point pt1, pt2;
-		double a = cos(theta), b = sin(theta);
-		double x0 = a*rho, y0 = b*rho;
-		pt1.x = cvRound(x0 + 1000*(-b));
-		pt1.y = cvRound(y0 + 1000*(a));
-		pt2.x = cvRound(x0 - 1000*(-b));
-		pt2.y = cvRound(y0 - 1000*(a));
-		if(abs(pt1.x - pt2.x) <= 5){
-			numOfLines++;
-			line( cedges, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
-			//printf("added red %d\n", pt1.x);
-		}
-	}
-	groupMin = 0.2*numOfLines;
-	if(groupMin < 1) groupMin = 1;
-
-	//printf("%d, %f\n", numOfLines, groupMin);
-
-	for( size_t i = 0; i < lines.size(); i++ )
-	{
-		float rho = lines[i][0], theta = lines[i][1];
-		double pt1, pt2;
-		double a = cos(theta), b = sin(theta);
-		double x0 = a*rho;
-		pt1 = cvRound(x0 + 1000*(-b));
-		pt2 = cvRound(x0 - 1000*(-b));
-		if(abs(pt1 - pt2) <= 5){
-			if(lastX == -1){
-				lastX = pt1;
-				Vec2f v = Vec2f(lines[i][0], lines[i][1]);
-				group1.push_back(v);
-			}
-			else{
-				if(abs(pt1 - lastX) <= 20){ //add to current group
-					lastX = pt1;
-					Vec2f v = Vec2f(lines[i][0], lines[i][1]);
-					(currGroup == 1) ? (group1.push_back(v)) : (group2.push_back(v));
-				}
-				else{	//need to create a new group;
-					if(currGroup == 1){
-						if(group1.size() >= groupMin){
-							currGroup++;
-							lastX = pt1;
-							Vec2f v = Vec2f(lines[i][0], lines[i][1]);
-							group2.push_back(v);
-						}
-						else{
-							group1.clear();
-							lastX = pt1;
-							Vec2f v = Vec2f(lines[i][0], lines[i][1]);
-							group1.push_back(v);
-						}
-					}
-					else{
-						if(group1.size() >= groupMin && group2.size() >=groupMin)
-							break;
-						else if(group1.size() >= groupMin){
-							group2.clear();
-							lastX = pt1;
-							Vec2f v = Vec2f(lines[i][0], lines[i][1]);
-							group2.push_back(v);
-							currGroup = 2;
-						}
-						else if(group2.size() >= groupMin){
-							group1.clear();
-							group1.insert(group1.begin(), group2.begin(), group2.end());
-							group2.clear();
-							lastX = pt1;
-							Vec2f v = Vec2f(lines[i][0], lines[i][1]);
-							group2.push_back(v);
-							currGroup = 2;
-						}
-					}
-				}
-			}
-		}
-	}
-
-	for( size_t i = 0; i < group1.size() || i < group2.size(); i++ ){
-		if(i < group1.size()){
-			float rho = group1[i][0], theta = group1[i][1];
-			Point pt1, pt2;
-			double a = cos(theta), b = sin(theta);
-			double x0 = a*rho, y0 = b*rho;
-			pt1.x = cvRound(x0 + 1000*(-b));
-			pt1.y = cvRound(y0 + 1000*(a));
-			pt2.x = cvRound(x0 - 1000*(-b));
-			pt2.y = cvRound(y0 - 1000*(a));
-			line( cedges, pt1, pt2, Scalar(255,0,0), 3, CV_AA);
-		}
-		if(i < group2.size()){
-			float rho = group2[i][0], theta = group2[i][1];
-			Point pt1, pt2;
-			double a = cos(theta), b = sin(theta);
-			double x0 = a*rho, y0 = b*rho;
-			pt1.x = cvRound(x0 + 1000*(-b));
-			pt1.y = cvRound(y0 + 1000*(a));
-			pt2.x = cvRound(x0 - 1000*(-b));
-			pt2.y = cvRound(y0 - 1000*(a));
-			line( cedges, pt1, pt2, Scalar(0,255,0), 3, CV_AA);
-		}
-	}
-
-	double totalLines = group1.size() + group2.size();
-	if(group1.size()/totalLines < groupsMinDiff)
-		group1.clear();
-	if(group2.size()/totalLines < groupsMinDiff){
-		group2.clear();
-	}
-
-	//if(abs(group1.size() - group2.size()) >
-
-	/// Using Canny's output as a mask, we display our result
-	Mat dst2;
-	dst2 = Scalar::all(0);
-
-	dst.copyTo( dst2, edges);
-	if(group1.size() > 0) numberOfLinesFound++;
-	if(group2.size() > 0) numberOfLinesFound++;
-	//Mat imgH;
-	//dst.convertTo(imgH, -1, 1.185, 10); //increase the contrast (double)
-	//imshow("Abs",dst);
-	//imshow("Abs", cedges);
+	waitKey(0);
+	return;
 }
