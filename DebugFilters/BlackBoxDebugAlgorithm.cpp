320ab895c28397f103b1ab502b03d6746f60c078
papigers <papigers93@gmail.com>
2015-05-11 23:00:21 +0300

    final polishes black box

--- DebugFilters/BlackBoxDebugAlgorithm.cpp
+++ DebugFilters/BlackBoxDebugAlgorithm.cpp
@@ -11,20 +11,21 @@ using namespace cv;
 using namespace std;
 
 /// Global variables
-Mat bb_src, bb_bw;
+Mat bb_src, bb_bw, drawing;
+//config file vars START HERE:
 int bb_thresh = 150;
 int bb_max_thresh = 255;
 RNG rng(12345);
+double minBlackResemblence = 5;
+//config file vars END HERE.
+string bb_picName = "";
+int numberOfBoxesFound = 0;
 
 /** Function Headers */
 void blackbox(int, void*);
 /** @function main */
 int boxDebug( int argc, char** argv )
 {
-	bb_src = imread( "black41.png" );
-
-	blackbox(0,0);
-	/*
 	DIR *dp;
 	struct dirent *dirp;
 	if((dp  = opendir(".")) == NULL) {
@@ -37,7 +38,7 @@ int boxDebug( int argc, char** argv )
 	while ((dirp = readdir(dp)) != NULL) {
 		string currFile = string(dirp->d_name);
 		if(currFile.find(".") != string::npos){
-			if(currFile.substr(0, 3).compare("box") == 0){
+			if(currFile.substr(0, 5).compare("black") == 0){
 				if(currFile.substr(currFile.find_last_of(".")).compare(".png") == 0){
 					filesToCheck.push_back(currFile);
 				}
@@ -48,16 +49,16 @@ int boxDebug( int argc, char** argv )
 	int successCounter = 0;
 	for(i = 0; i < filesToCheck.size(); i++){
 		string pic = filesToCheck[i];
-		int numberOfLinesInFile = (int)(pic[3]-'0');
-		src = imread( pic );
-		if( !src.data )
+		int numberOfBoxesInFile = (int)(pic[5]-'0');
+		bb_src = imread( pic );
+		if( !bb_src.data )
 		{ return -1; }
-		picName = pic;
-		abs(0, 0);
-		if(numberOfLinesInFile != numberOfLinesFound){
-			printf("We found %d lines instead of %d in file %s\n",numberOfLinesFound,numberOfLinesInFile,pic.c_str());
+		bb_picName = pic;
+		blackbox(0, 0);
+		if(numberOfBoxesInFile != numberOfBoxesFound){
+			printf("We found %d lines instead of %d in file %s\n",numberOfBoxesFound,numberOfBoxesInFile,pic.c_str());
 			namedWindow(pic.c_str(), CV_WINDOW_AUTOSIZE );
-			imshow(pic.c_str(), cedges);
+			imshow(pic.c_str(), drawing);
 		}
 		else
 			successCounter++;
@@ -67,7 +68,6 @@ int boxDebug( int argc, char** argv )
 	printf("We were correct in %f%% of the cases\n",successRate);
 
 	waitKey(0);
-	 */
 	return 0;
 }
 
@@ -94,9 +94,9 @@ void thresh_callback(int, void* ){
 		minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
 	}
 
-	double minBlackResemblence = 5;
+	minBlackResemblence = 5;
 	vector<bool> draw(contours.size()) ;
-	int boxesFound = 0;
+	numberOfBoxesFound = 0;
 	for( uint i = 0; i < contours.size(); i++ )
 	{
 		Mat image_roi = bb_src(boundRect[i]);
@@ -107,7 +107,7 @@ void thresh_callback(int, void* ){
 		bool good = abs(image_roi.cols- bb_src.cols)>5 &&abs(image_roi.rows- bb_src.rows)>5;
 		if (dist >= minBlackResemblence && good){
 			draw[i]=true;
-			boxesFound++;
+			numberOfBoxesFound++;
 		}
 		else
 			draw[i]=false;
@@ -115,7 +115,7 @@ void thresh_callback(int, void* ){
 
 
 	/// Draw polygonal contour + bonding rects + circles
-	Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
+	drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
 	for( uint i = 0; i< contours.size(); i++ )
 	{
 		if(draw[i]){
@@ -123,6 +123,19 @@ void thresh_callback(int, void* ){
 			drawContours( drawing, contours_poly, i, color, 2, 8, vector<Vec4i>(), 0, Point() );
 			rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), Scalar(255, 0, 0), 1, 8, 0 );
 			circle( drawing, center[i], (int)radius[i], Scalar(0, 255, 0), 1, 8, 0 );
+		}
+	}
+
+	//TODO:
+	//send back to ros part:
+	for( uint i = 0; i< contours.size(); i++ )
+	{
+		if(draw[i]){
+			//need to return:
+			Point topLeft =boundRect[i].tl();
+			Point bottomRight = boundRect[i].br();
+			Point topRight = Point(bottomRight.x, topLeft.y);
+			Point bottomLeft = Point(topLeft.x, bottomRight.y);
 		}
 	}
 
@@ -164,15 +177,15 @@ void thresh_callback(int, void* ){
 	}
 	 */
 	/// Show in a window
-	namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-	imshow( "Contours", drawing );
+	//namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+	//imshow( "Contours", drawing );
 	//namedWindow( "Distance", CV_WINDOW_AUTOSIZE );
 	//imshow( "Distance", dists );
 }
 
 void blackbox(int, void*){
-	namedWindow( "Source", CV_WINDOW_AUTOSIZE );
-	imshow( "Source", bb_src );
+	//namedWindow( "Source", CV_WINDOW_AUTOSIZE );
+	//imshow( "Source", bb_src );
 	cvtColor(bb_src,bb_bw, CV_RGB2GRAY);
 	blur(bb_bw, bb_bw, Size(3,3));
 
